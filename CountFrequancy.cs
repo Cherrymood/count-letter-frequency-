342aaa30d936aabd880e11f14c4280a5ff012845
Valeriia <valeriia@Sergeys-MacBook-Pro.local>
2023-10-24 10:50:13 -0700

    Added CountFreq class and members

---
+++ CountFrequancy.cs
@@ -0,0 +1,14 @@
+class CountFrequancy{
+
+    Dictionary Count = new Dictionary<string, int>();
+    string alphabeth = "abcdefghijklmnopqrstuvwxyz";
+
+    public Dictionary<string, int> CountFre (string text)
+    {
+        text.ToLower();
+
+        
+    }
+
+
+}
