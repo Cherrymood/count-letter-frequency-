b1615e808d59dd9607d9f8cacd7a8bdc2eced28b
Valeriia <valeriia@Sergeys-MacBook-Pro.local>
2023-10-24 17:12:55 -0700

    Renamed members

--- Application.cs
+++ Application.cs
@@ -1,26 +1,32 @@
 class Application{
 
-    ReadText sherlockText;
+    ReadText textToCompare;
     CountFrequency freqText;
+    EnglishAlphabetFreq compareText;
 
     public Application()
     {
         // Initialize the ReadText object
-        sherlockText = new ReadText(); 
+        textToCompare = new ReadText(); 
 
         // Initialize the CountFrequency object
         freqText = new CountFrequency(); 
+
+        // Initialize the EnglishAlphabetFreq object
+        compareText = new EnglishAlphabetFreq(); 
+
     }
 
     public Dictionary<char, double> Execute(string pathText)
     {
-        var sherlock = sherlockText.ReadFile(pathText);
-        var textFreq = freqText.Count(sherlock);
+        var text = textToCompare.ReadFile(pathText);
+        var textFreq = freqText.Count(text);
+        var compareText = compare.CompareEnglishDict(textFreq);
         // Return the textFreq dictionary
-        foreach (var kvp in textFreq)
+        foreach (var kvp in compareFreq)
         {
             Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
         }
-        return textFreq; 
+        return compareFreq; 
     }
 }
